7f309f470363e297fde0624705f64332fc654837
T4ngi <ferdhant10@gmail.com>
2024-10-09 18:00:48 +0700

    codelab modul 2

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_application_1/app/modules/http_screen/bindings/http_binding.dart';
+import 'package:flutter_application_1/app/modules/http_screen/views/http_view.dart';
+import 'package:get/get.dart';
+
+
+import '../modules/article_detail/bindings/article_detail_bindings.dart';
+import '../modules/article_detail/views/article_detail_web_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/article_detail.dart';
+import '../modules/home/views/home_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+        name: _Paths.HTTP,
+        page: () => const HttpView(),
+        binding: HttpBinding()),
+    GetPage(
+        name: _Paths.ARTICLE_DETAILS,
+        page: () => ArticleDetailPage(article: Get.arguments),
+        binding: ArticleDetailBinding()),
+    GetPage(
+        name: _Paths.ARTICLE_DETAILS_WEBVIEW,
+        page: () => ArticleDetailWebView(article: Get.arguments),
+        binding: ArticleDetailBinding())
+  ];
+}
